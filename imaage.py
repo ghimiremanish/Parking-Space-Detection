181668d5f82ebabdd1dd127f9e5eec30b2ec6a7b
ghimiremanish <manishghimire153200@gmail.com>
2018-12-01 19:28:18 +0545

    all data plotted in image sucessfully

--- imaage.py
+++ imaage.py
@@ -1,18 +1,15 @@
 import cv2
 import numpy as np
-from loadCo import loadCo
+from loadCoordinates import loadCo
 
 c = loadCo()
+data = c.output()
 
 img = cv2.imread('images/image.jpg',1)
 
-
-data = c.output()
 main = []
 count = 0
-
 for i in data:
-    print i
     if count == 0:
         x1 = i
     elif count == 1:
@@ -30,25 +27,15 @@ for i in data:
     elif count == 7:
         y4 = i
         main.append(((x1, y1), (x2, y2), (x3, y3), (x4, y4)))
-        count = 0
+        count = -1
     count = count + 1
 
-
-# print data
-
-# for i in data:
-#     print i
-
-# for m in main:
-#     vrx = np.array(m, np.int32)
-#     vrx = vrx.reshape((-1,1,2))
-#     img = cv2.polylines(img, [vrx], True, (0,255,255),3)
-
-# vrx = np.array(main, np.int32)
-# vrx = vrx.reshape((-1,1,2))
-# img = cv2.polylines(img, [vrx], True, (0,255,255),3)
+for m in main:
+    vrx = np.array(m, np.int32)
+    vrx = vrx.reshape((-1,1,2))
+    img = cv2.polylines(img, [vrx], True, (0,255,255),3)
 
 
-# cv2.imshow('image',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ loadCoordinates.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+class loadCo:
+    f = open('coordinates.txt','r')
+
+    def output(self):
+
+        points = self.f.read()
+        
+        s = points.split()
+        arr = []
+        for a in s:
+            arr.append(int(a))
+
+
+        return arr
+        # c = 0
+        # for i in points:
+        #     if i != '':
+        #         print 'space found'
+        #         c = c+1
+        #         print c
+        #     # print i
+
+        # data manupulating to array
+#         b = ''
+
+#         #for storing stings
+#         val = []
+#         # final array list of polygon 
+#         arr = []
+
+#         #count
+#         c = 0
+
+#         for p in points:
+#             #removing impurities from text to get numbers
+#             if p != '[' and p != '(' and p != ',' and p != ')' and p != ' ' and p != ']':
+#                 val.append(p)
+
+        
+#         # for i in val:
+#         #     b = b + i
+#         #     c = c + 1
+#         #     if c == 3:
+#         #         arr.append(int(b))
+#         #         b = ''
+#         #         c = 0
+        
+#         # return arr
+
+
+# c = loadCo()
+# # print(c.output())
+# f = c.output()
+
+# print f
